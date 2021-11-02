3887fdc4cb2f1828f602ae6f21e83ca6c31ce7af
Robert Salca <robert.salca@gmail.com>
2021-11-02 15:09:59 +0000

    write utils tests for geojson conversion

--- db/utils/geojson.utils.js
+++ db/utils/geojson.utils.js
@@ -1,21 +1,31 @@
 exports.geojsonCollectionToPostgis = (geojson) => {
-  return geojson.features.map((feat) => {
-    if (feat.geometry.type === "Point") {
-      return [
-        `${feat.geometry.type}`,
-        `${feat.geometry.type}(${feat.geometry.coordinates[0]} ${feat.geometry.coordinates[1]})`,
-      ];
-    } else if (feat.geometry.type === "LineString") {
-      return [
-        `${feat.geometry.type}`,
-        `${feat.geometry.type}(${feat.geometry.coordinates[0][0]} ${feat.geometry.coordinates[0][1]}, ${feat.geometry.coordinates[1][0]} ${feat.geometry.coordinates[1][1]})`,
-      ];
-    } else if (feat.geometry.type === "Polygon") {
-      var tempStr = ``;
-      return [
-        `${feat.geometry.type}`,
-        `${feat.geometry.type}(${feat.geometry.coordinates[0][0][0]} ${feat.geometry.coordinates[0][0][1]}, ${feat.geometry.coordinates[0][1][0]} ${feat.geometry.coordinates[0][1][1]})`,
-      ];
-    }
-  });
+	return geojson.features.map((feat) => {
+		if (feat.geometry.type === 'Point') {
+			return [
+				`POINT(${feat.geometry.coordinates[0]} ${feat.geometry.coordinates[1]})`,
+			];
+		} else if (feat.geometry.type === 'LineString') {
+			var tempStr = `LINESTRING(`;
+			feat.geometry.coordinates.forEach((point) => {
+				tempStr += `${point[0]} ${point[1]},`;
+			});
+			tempStr = tempStr.slice(0, -1);
+			tempStr += `)`;
+			return [tempStr];
+		} else if (feat.geometry.type === 'Polygon') {
+			var tempStr = `POLYGON(`;
+			feat.geometry.coordinates.forEach((polygon) => {
+				var polStr = `(`;
+				polygon.forEach((point) => {
+					polStr += `${point[0]} ${point[1]},`;
+				});
+				polStr = polStr.slice(0, -1);
+				polStr += `),`;
+				tempStr += polStr;
+			});
+			tempStr = tempStr.slice(0, -1);
+			tempStr += `)`;
+			return [tempStr];
+		}
+	});
 };
