3ff7de27ccc91307f4809abf2a0f5260c804b219
Andrew Richardson (W) <andrew-w.richardson@BLFML3TTML85.local>
2021-11-05 15:09:14 +0000

    add additional files

--- db/utils/public-api-dataclip.util.js
+++ db/utils/public-api-dataclip.util.js
@@ -1,14 +1,12 @@
 const turf = require("@turf/turf");
-
-const receptorData = require(`../data/test-data/receptors`);
+const { jsToPgFormatReceptors } = require(`./data-manipulation.utils`);
 const db = require("../connection");
+const format = require("pg-format");
+fs = require("fs");
 
-exports.dataclip = async (geojson, project_id) => {
-  //console.log(project_id);
-  //console.log("geojson--->", geojson);
-
+exports.dataclip = async (geojson, project_id, api_id) => {
   const publicApis = await db.query(`SELECT * FROM public_apis;`);
-  //console.log("publicAPI--->", publicApis);
+
   const assessmentArea = await db.query(
     `SELECT json_build_object(
         'type', 'FeatureCollection',
@@ -17,14 +15,10 @@ exports.dataclip = async (geojson, project_id) => {
          WHERE project_id = $1;`,
     [project_id]
   );
-  //   console.log(
-  //     assessmentArea.rows[0].json_build_object.features,
-  //     "assessment area"
-  //   );
-  let polygon = assessmentArea.rows[0].json_build_object;
-  //clip points
 
-  //   //// format points
+  let assessmentAreaPolygon = assessmentArea.rows[0].json_build_object;
+
+  //// format points
   const points = [];
   geojson.features.forEach((feature) => {
     if (feature.geometry.type === "Point") {
@@ -34,77 +28,93 @@ exports.dataclip = async (geojson, project_id) => {
 
   const ptsWithin = turf.pointsWithinPolygon(
     turf.featureCollection(points),
-    polygon
+    assessmentAreaPolygon
   );
 
-  //// format lines
+  const allFeatures = ptsWithin;
+
+  //// find overlapping lines
   const lineString = [];
   geojson.features.forEach((feature) => {
     if (feature.geometry.type === "LineString") {
       lineString.push(feature);
     }
   });
-  //   console.log(lineString[0].geometry.coordinates, "----<");
-  //   // determine intersection points
-  //   let intersects = turf.lineIntersect(
-  //     turf.featureCollection(lineString),
-  //     polygon
-  //   );
-  //   console.log(intersects.features[0].geometry, "---->intersects");
-
-  //split line feature
 
   const splitArr = [];
   lineString.forEach((feature) => {
-    polygon.features.forEach((polygon) => {
-      const split = turf.lineSplit(feature, polygon);
-      console.log(split, "split");
-      split.features.forEach((obj) => {
-        obj.properties = feature.properties;
-        splitArr.push(obj);
-      });
+    assessmentAreaPolygon.features.forEach((assessmentAreaPolygon) => {
+      if (turf.booleanWithin(feature, assessmentAreaPolygon)) {
+        allFeatures.features.push(feature);
+      } else {
+        const split = turf.lineSplit(feature, assessmentAreaPolygon);
+        split.features.forEach((obj) => {
+          obj.properties = feature.properties;
+          obj.id = feature.id;
+          splitArr.push(obj);
+        });
+      }
     });
   });
-  console.log(splitArr, "<<<<");
+
+  splitArr.forEach((splitLines) => {
+    assessmentAreaPolygon.features.forEach((assessmentAreaPolygon) => {
+      let length = turf.length(splitLines);
+      let center = turf.along(splitLines, length);
+      let overlapping = turf.booleanWithin(center, assessmentAreaPolygon);
+
+      if (overlapping == true) {
+        allFeatures.features.push(splitLines);
+        console.log(overlapping, splitLines, "<-----");
+      }
+    });
+  });
 
   //find overlapping polygon
-  const overlappingArr = [];
-  splitArr.forEach((splitLines) => {
-    polygon.features.forEach((polygon) => {
-      let overlapping = turf.lineOverlap(polygon, splitLines);
-      console.log(polygon, "polygon");
-      console.log(splitLines, "splitlines");
-      console.log(overlapping);
-      overlappingArr.push(overlapping);
+  const polygons = [];
+  geojson.features.forEach((feature) => {
+    if (feature.geometry.type === "Polygon") {
+      polygons.push(feature);
+    }
+  });
+
+  const polygonsWthn = [];
+  polygons.forEach((polygon) => {
+    assessmentAreaPolygon.features.forEach((assessmentAreaPolygon) => {
+      let intersection = turf.intersect(polygon, assessmentAreaPolygon);
+
+      if (intersection !== null) {
+        intersection.type = polygon.type;
+        intersection.properties = polygon.properties;
+        intersection.id = polygon.id;
+        allFeatures.features.push(intersection);
+      }
     });
   });
-  console.log(overlappingArr, "overlappingArr");
-  return assessmentArea;
 
-  //dataclip(receptorData, 1);
+  const receptors = [
+    {
+      project_id: project_id,
+      api_id: api_id,
+      geometry: allFeatures,
+    },
+  ];
 
-  //  //// format points
-  //  const points = [];
-  //  geojson.features.forEach((feature) => {
-  //    if (feature.geometry.type === "Point") {
-  //      points.push(feature.geometry.coordinates);
-  //    }
-  //  });
-  //  console.log(points);
+  console.log(receptors, "receptors");
+  fs.writeFile(`receptors.txt`, JSON.stringify(receptors), function (err) {
+    if (err) return console.log(err);
+  });
+  return receptors;
+};
 
-  //  ////format polygon
-  //  const polygon = [];
-  //  assessmentArea.rows[0].json_build_object.features.forEach((feature) => {
-  //    feature.geometry.coordinates.forEach((location) => {
-  //      polygon.push(location);
-  //    });
-  //  });
+exports.insertReceptorsData = async (receptors) => {
+  const formattedReceptors = jsToPgFormatReceptors(receptors);
+  queryString = format(
+    `INSERT INTO receptors (project_id, api_id, geom,osm_id, type, properties) VALUES %L RETURNING *;`,
+    formattedReceptors
+  );
 
-  //  console.log(polygon, " ---> polygon");
+  const log = await db.query(queryString);
 
-  //  const ptsWithin = turf.pointsWithinPolygon(
-  //    turf.points(points,),
-  //    turf.polygon(polygon)
-  //  );
-  //  console.log(ptsWithin.features[0].geometry, "within")
+  await db.query(`select* from receptors;`);
 };
--- models/public-apis.models.js
+++ models/public-apis.models.js
@@ -1,6 +1,24 @@
 const { dataclip } = require("../db/utils/public-api-dataclip.util");
+let osmtogeojson = require("osmtogeojson");
 
-exports.retrieveOSMRivers = async (geojson, project_id) => {
-  const result = await dataclip(geojson, project_id);
+//OSM API
+const OSMApi = axios.create({
+  baseURL: "https://lz4.overpass-api.de/api",
+});
+
+exports.retrieveOSMRivers = async (
+  project_id,
+  minLat,
+  maxLat,
+  minLong,
+  maxLat
+) => {
+  const { data } = await OSMApi.get(
+    `https://lz4.overpass-api.de/api/interpreter/?data=[out:json][timeout:100];(node["waterway"](54.58247761244,-5.9683012962341,54.584942850744,-5.9637254476547);way["waterway"](54.58247761244,-5.9683012962341,54.584942850744,-5.9637254476547);relation["waterway"](54.58247761244,-5.9683012962341,54.584942850744,-5.9637254476547););out;>;out skel qt;`
+  );
+
+  const result = await dataclip(data, project_id);
+  const convertedData = osmtogeojson(result);
+
   return result;
 };
