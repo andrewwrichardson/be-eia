218980a71be209f9a678d2a491027cf95530ab9f
Robert Salca <robert.salca@gmail.com>
2021-11-05 09:53:03 +0000

    GET /api/comments/receptors-comments/:project_id

--- __tests__/comments.test.js
+++ __tests__/comments.test.js
@@ -9,93 +9,103 @@ beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
 describe('GET /api/comments/receptors-comments/:project_id', () => {
-	test('200: comments', () => {
-		return request(app)
-			.get('/api/comments/receptors-comments/1')
-			.expect(200)
-			.then((result) => {
-				console.log(result.body);
-			});
-	});
+    test('200: comments', () => {
+        return request(app)
+            .get('/api/comments/receptors-comments/1')
+            .expect(200)
+            .then((result) => {
+                result.body.comments.forEach((comm) => {
+                    expect(comm).toEqual({
+                        receptor_id: expect.any(Number),
+                        project_id: expect.any(Number),
+                        api_id: expect.any(Number),
+                        osm_id: expect.any(String),
+                        type: expect.any(String),
+                        properties: expect.any(Object),
+                        geometry: expect.any(Object),
+                    });
+                });
+            });
+    });
 });
 
 describe('POST /api/comments', () => {
-	test('201: created ', () => {
-		return request(app)
-			.post('/api/comments')
-			.send({
-				newComment: {
-					receptor_id: 1,
-					impact: 'Minor',
-					comment: 'yes no yes no',
-				},
-			})
-			.expect(201)
-			.then((result) => {
-				expect(result.body.comment).toEqual({
-					comment_id: 3,
-					receptor_id: 1,
-					impact: 'Minor',
-					comment: 'yes no yes no',
-				});
-			});
-	});
+    test('201: created ', () => {
+        return request(app)
+            .post('/api/comments')
+            .send({
+                newComment: {
+                    receptor_id: 1,
+                    impact: 'Minor',
+                    comment: 'yes no yes no',
+                },
+            })
+            .expect(201)
+            .then((result) => {
+                expect(result.body.comment).toEqual({
+                    comment_id: 3,
+                    receptor_id: 1,
+                    impact: 'Minor',
+                    comment: 'yes no yes no',
+                });
+            });
+    });
 });
 
 describe('PATCH /api/comments/:comment_id', () => {
-	test('201: created - both impact and comment', () => {
-		return request(app)
-			.patch('/api/comments/1')
-			.send({
-				updatedComment: {
-					impact: 'Major',
-					comment: 'yes no yes no',
-				},
-			})
-			.expect(201)
-			.then((result) => {
-				expect(result.body.comment).toEqual({
-					comment_id: 1,
-					receptor_id: 1,
-					impact: 'Major',
-					comment: 'yes no yes no',
-				});
-			});
-	});
-	test('201: created - impact', () => {
-		return request(app)
-			.patch('/api/comments/1')
-			.send({
-				updatedComment: {
-					impact: 'Major',
-				},
-			})
-			.expect(201)
-			.then((result) => {
-				expect(result.body.comment).toEqual({
-					comment_id: 1,
-					receptor_id: 1,
-					impact: 'Major',
-					comment: 'bla bla bla bla bla',
-				});
-			});
-	});
-	test('201: created - comment', () => {
-		return request(app)
-			.patch('/api/comments/1')
-			.send({
-				updatedComment: {
-					comment: 'yes no yes no',
-				},
-			})
-			.expect(201)
-			.then((result) => {
-				expect(result.body.comment).toEqual({
-					comment_id: 1,
-					receptor_id: 1,
-					impact: 'Moderate',
-					comment: 'yes no yes no',
-				});
-			});
-	});
+    test('201: created - both impact and comment', () => {
+        return request(app)
+            .patch('/api/comments/1')
+            .send({
+                updatedComment: {
+                    impact: 'Major',
+                    comment: 'yes no yes no',
+                },
+            })
+            .expect(201)
+            .then((result) => {
+                expect(result.body.comment).toEqual({
+                    comment_id: 1,
+                    receptor_id: 1,
+                    impact: 'Major',
+                    comment: 'yes no yes no',
+                });
+            });
+    });
+    test('201: created - impact', () => {
+        return request(app)
+            .patch('/api/comments/1')
+            .send({
+                updatedComment: {
+                    impact: 'Major',
+                },
+            })
+            .expect(201)
+            .then((result) => {
+                expect(result.body.comment).toEqual({
+                    comment_id: 1,
+                    receptor_id: 1,
+                    impact: 'Major',
+                    comment: 'bla bla bla bla bla',
+                });
+            });
+    });
+    test('201: created - comment', () => {
+        return request(app)
+            .patch('/api/comments/1')
+            .send({
+                updatedComment: {
+                    comment: 'yes no yes no',
+                },
+            })
+            .expect(201)
+            .then((result) => {
+                expect(result.body.comment).toEqual({
+                    comment_id: 1,
+                    receptor_id: 1,
+                    impact: 'Moderate',
+                    comment: 'yes no yes no',
+                });
+            });
+    });
 });
--- models/comments.models.js
+++ models/comments.models.js
@@ -1,56 +1,65 @@
 const db = require('../db/connection');
 
 exports.fetchComments = async (project_id) => {
-	let queryStr = `SELECT * FROM receptors
+    let queryStr = `SELECT receptors.receptor_id, receptors.project_id, receptors.api_id, receptors.osm_id, receptors.type, receptors.properties, json_build_object('type', 'FeatureCollection', 'features', json_agg(json_build_object('type','Feature','properties',json_build_object(),'geometry',ST_AsGeoJSON(receptors.geom)::json))) FROM receptors
     INNER JOIN comments ON receptors.receptor_id = comments.comment_id
-    WHERE project_id = $1;`;
-	let queryVals = [project_id];
+    WHERE project_id = $1 GROUP BY receptors.receptor_id;`;
+    let queryVals = [project_id];
 
-	const result = await db.query(queryStr, queryVals);
+    const result = await db.query(queryStr, queryVals);
 
-	if (result.rows.length === 0) {
-		return Promise.reject({ status: 404, msg: 'Not Found' });
-	}
+    if (result.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: 'Not Found' });
+    }
 
-	return result.rows;
+    const updatedComments = [];
+    result.rows.forEach((comment) => {
+        let updatedComment = { ...comment };
+        delete updatedComment.json_build_object;
+        updatedComment.properties = JSON.parse(updatedComment.properties);
+        updatedComment.geometry = { ...comment.json_build_object };
+        updatedComments.push(updatedComment);
+    });
+
+    return updatedComments;
 };
 
 exports.addComment = async (newComment) => {
-	let queryStr = `INSERT INTO comments (receptor_id, impact, comment) VALUES ($1, $2, $3) RETURNING *;`;
-	let queryVals = [
-		newComment.receptor_id,
-		newComment.impact,
-		newComment.comment,
-	];
+    let queryStr = `INSERT INTO comments (receptor_id, impact, comment) VALUES ($1, $2, $3) RETURNING *;`;
+    let queryVals = [
+        newComment.receptor_id,
+        newComment.impact,
+        newComment.comment,
+    ];
 
-	const result = await db.query(queryStr, queryVals);
+    const result = await db.query(queryStr, queryVals);
 
-	if (result.rows.length === 0) {
-		return Promise.reject({ status: 404, msg: 'Not Found' });
-	}
+    if (result.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: 'Not Found' });
+    }
 
-	return result.rows[0];
+    return result.rows[0];
 };
 
 exports.editComment = async (comment_id, updatedComment) => {
-	if (
-		updatedComment.impact !== undefined &&
-		updatedComment.comment !== undefined
-	) {
-		queryStr = `UPDATE comments SET impact = $1, comment = $2 WHERE comment_id = $3 RETURNING *;`;
-		queryVals = [updatedComment.impact, updatedComment.comment, comment_id];
-	} else if (updatedComment.impact !== undefined) {
-		queryStr = `UPDATE comments SET impact = $1 WHERE comment_id = $2 RETURNING *;`;
-		queryVals = [updatedComment.impact, comment_id];
-	} else if (updatedComment.comment !== undefined) {
-		queryStr = `UPDATE comments SET comment = $1 WHERE comment_id = $2 RETURNING *;`;
-		queryVals = [updatedComment.comment, comment_id];
-	}
-	const result = await db.query(queryStr, queryVals);
+    if (
+        updatedComment.impact !== undefined &&
+        updatedComment.comment !== undefined
+    ) {
+        queryStr = `UPDATE comments SET impact = $1, comment = $2 WHERE comment_id = $3 RETURNING *;`;
+        queryVals = [updatedComment.impact, updatedComment.comment, comment_id];
+    } else if (updatedComment.impact !== undefined) {
+        queryStr = `UPDATE comments SET impact = $1 WHERE comment_id = $2 RETURNING *;`;
+        queryVals = [updatedComment.impact, comment_id];
+    } else if (updatedComment.comment !== undefined) {
+        queryStr = `UPDATE comments SET comment = $1 WHERE comment_id = $2 RETURNING *;`;
+        queryVals = [updatedComment.comment, comment_id];
+    }
+    const result = await db.query(queryStr, queryVals);
 
-	if (result.rows.length === 0) {
-		return Promise.reject({ status: 404, msg: 'Not Found' });
-	}
+    if (result.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: 'Not Found' });
+    }
 
-	return result.rows[0];
+    return result.rows[0];
 };
