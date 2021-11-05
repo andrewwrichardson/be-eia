fef7c5f286b3bd0d72b8c8808b16407e883e84cd
Andrew Richardson (W) <andrew-w.richardson@BLFML3TTML85.local>
2021-11-05 21:51:50 +0000

    add river alignment api

--- __tests__/public-apis.test.js
+++ __tests__/public-apis.test.js
@@ -3,14 +3,14 @@ const db = require("../db/connection");
 const { seed } = require("../db/seeds/seed");
 const testData = require("../db/data/test-data/index");
 
-const postApiData = require("../models/public-apis.models");
+const retrieveOSMRivers = require("../models/public-apis.models");
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
 describe("populate receptor from public API", () => {
-  test("should not mutate", () => {
-    const result = postApiData(1);
+  test("should not mutate", async () => {
+    const result = await retrieveOSMRivers(1);
     console.log(result, "result");
     expect(result).toEqual(1);
   });
--- models/public-apis.models.js
+++ models/public-apis.models.js
@@ -1,24 +1,38 @@
 const { dataclip } = require("../db/utils/public-api-dataclip.util");
 let osmtogeojson = require("osmtogeojson");
-
+const turf = require("@turf/turf");
+let axios = require("axios");
+const db = require("../db/connection");
 //OSM API
 const OSMApi = axios.create({
   baseURL: "https://lz4.overpass-api.de/api",
 });
 
-exports.retrieveOSMRivers = async (
-  project_id,
-  minLat,
-  maxLat,
-  minLong,
-  maxLat
-) => {
-  const { data } = await OSMApi.get(
-    `https://lz4.overpass-api.de/api/interpreter/?data=[out:json][timeout:100];(node["waterway"](54.58247761244,-5.9683012962341,54.584942850744,-5.9637254476547);way["waterway"](54.58247761244,-5.9683012962341,54.584942850744,-5.9637254476547);relation["waterway"](54.58247761244,-5.9683012962341,54.584942850744,-5.9637254476547););out;>;out skel qt;`
+const retrieveOSMRivers = async (project_id) => {
+  const publicApis = await db.query(`SELECT * FROM public_apis;`);
+
+  const assessmentArea = await db.query(
+    `SELECT json_build_object(
+        'type', 'FeatureCollection',
+        'features', json_agg(json_build_object('type','Feature','properties',json_build_object(),'geometry',ST_AsGeoJSON(assessment_areas.geom)::json)))
+         FROM assessment_areas
+         WHERE project_id = $1;`,
+    [project_id]
   );
 
-  const result = await dataclip(data, project_id);
-  const convertedData = osmtogeojson(result);
+  let assessmentAreaPolygon =
+    assessmentArea.rows[0].json_build_object.features[0].geometry.coordinates;
+  console.log(assessmentAreaPolygon, "aa");
+  let boundingBox = turf.bboxPolygon(assessmentAreaPolygon);
+  console.log(boundingBox, "bb");
 
-  return result;
+  // const { data } = await OSMApi.get(
+  //   `https://lz4.overpass-api.de/api/interpreter/?data=[out:json][timeout:100];(node["waterway"](54.58247761244,-5.9683012962341,54.584942850744,-5.9637254476547);way["waterway"](54.58247761244,-5.9683012962341,54.584942850744,-5.9637254476547);relation["waterway"](54.58247761244,-5.9683012962341,54.584942850744,-5.9637254476547););out;>;out skel qt;`
+  // );
+
+  // const result = await dataclip(d∂ata, project_id);
+  // const convertedData = osmtogeojson(result);
+
+  // return result;
 };
+retrieveOSMRivers(1);
