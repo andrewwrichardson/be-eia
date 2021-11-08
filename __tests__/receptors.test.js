6de3a16a6ca465d1c4fa1485cab83f2f577f11f8
Robert Salca <robert.salca@gmail.com>
2021-11-08 10:19:27 +0000

    update on endpoints list

--- __tests__/receptors.test.js
+++ __tests__/receptors.test.js
@@ -8,23 +8,23 @@ const request = require('supertest');
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
-describe('GET /api/receptors:project_id', () => {
-	test('200: retrieve all receptors for project_id', () => {
-		return request(app)
-			.get('/api/receptors/1')
-			.expect(200)
-			.then((result) => {
-				result.body.receptors.forEach((receptor) => {
-					expect(receptor).toEqual({
-						receptor_id: expect.any(Number),
-						project_id: expect.any(Number),
-						api_id: expect.any(Number),
-						osm_id: expect.any(String),
-						type: expect.any(String),
-						properties: expect.any(Object),
-						geometry: expect.any(Object),
-					});
-				});
-			});
-	});
+describe('GET /api/receptors/:project_id', () => {
+    test('200: retrieve all receptors for project_id', () => {
+        return request(app)
+            .get('/api/receptors/1')
+            .expect(200)
+            .then((result) => {
+                result.body.receptors.forEach((receptor) => {
+                    expect(receptor).toEqual({
+                        receptor_id: expect.any(Number),
+                        project_id: expect.any(Number),
+                        api_id: expect.any(Number),
+                        osm_id: expect.any(String),
+                        type: expect.any(String),
+                        properties: expect.any(Object),
+                        geometry: expect.any(Object),
+                    });
+                });
+            });
+    });
 });
