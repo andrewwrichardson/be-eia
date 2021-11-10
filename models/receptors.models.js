3aa2089ec3cb8fea816bd6ef36ed7537568cbd12
Robert Salca <robert.salca@gmail.com>
2021-11-10 15:17:46 +0000

    receptors model bug fix

--- models/receptors.models.js
+++ models/receptors.models.js
@@ -2,7 +2,7 @@ const db = require('../db/connection');
 
 exports.fetchReceptors = async (project_id) => {
     let queryStr = `SELECT receptor_id, project_id, api_id, osm_id, type, properties, json_build_object('type', 'FeatureCollection', 'features', json_agg(json_build_object('type','Feature','properties',json_build_object(),'geometry',ST_AsGeoJSON(receptors.geom)::json))) FROM receptors WHERE project_id = $1 GROUP BY receptor_id;`;
-    let queryVals = [1];
+    let queryVals = [project_id];
 
     const result = await db.query(queryStr, queryVals);
 
