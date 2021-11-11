c1d1feeaf2306abf1112ce4d66f5cf42069ca16e
Andrew Richardson (W) <andrew-w.richardson@BLFML3TTML85.local>
2021-11-11 13:00:21 +0000

    remove steven from projects

--- db/data/development-data/projects.js
+++ db/data/development-data/projects.js
@@ -1,10 +1,12 @@
 module.exports = [
-	{
-		project_name: 'Project One',
-		image_url: 'https://www.stevensegallery.com/140/100',
-	},
-	{
-		project_name: 'Project Two',
-		image_url: 'https://www.stevensegallery.com/140/100',
-	},
+    {
+        project_name: 'Kurilpa Bridge',
+        image_url:
+            'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dezeen.com%2F2015%2F12%2F11%2Fdezeen-a-z-advent-calendar-kurilpa-bridge-cox-rayner-architects-architectures-arup-brisbane-australia%2F&psig=AOvVaw3287JsOkK4FtMVyErbM81X&ust=1636721669630000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOiXrOmtkPQCFQAAAAAdAAAAABAP',
+    },
+    {
+        project_name: 'Stonecutters Bridge',
+        image_url:
+            'http://www.aeol.com.au/databases/news/15/02/images/iqpc_1507.jpg',
+    },
 ];
